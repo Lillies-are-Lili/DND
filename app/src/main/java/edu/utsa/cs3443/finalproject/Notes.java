2fd092756f053f363c594725d751eeedadca7db6
Mack <mack.j.tully@gmail.com>
2023-11-12 16:10:48 -0600

    Test

---
+++ app/src/main/java/edu/utsa/cs3443/finalproject/Notes.java
@@ -0,0 +1,114 @@
+package edu.utsa.cs3443.finalproject;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import android.app.Activity;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ * The Notes class manages the creation, loading, and manipulation of notes.
+ */
+public class Notes {
+    private String note;
+    private Activity activity;
+    private File[] listOfFiles;
+
+    /**
+     * Constructor for the Notes class.
+     *
+     * @param activity The activity associated with the Notes instance.
+     */
+    public Notes(Activity activity) {
+        this.activity = activity;
+    }
+
+    /**
+     * Load notes from the notes directory.
+     */
+    public void loadNotes() {
+        File directory = new File(activity.getApplicationContext().getFilesDir().toString() + "/notes");
+        if(directory.exists()){
+            listOfFiles = directory.listFiles();
+        }else{
+            directory.mkdir();
+        }
+    }
+
+    /**
+     * Get the names of all available notes.
+     *
+     * @return An ArrayList containing the names of all notes.
+     */
+    public ArrayList<String> getnames() {
+        ArrayList<String> str = new ArrayList<>();
+        for(File file : listOfFiles){
+            str.add(file.getName());
+        }
+        return str;
+    }
+
+    /**
+     * Get the content of a specific note.
+     *
+     * @param indexOf The index of the note in the list of files.
+     * @return The content of the specified note.
+     */
+    public String getText(int indexOf) {
+        String str = "";
+        try {
+            FileReader reader = new FileReader(listOfFiles[indexOf]);
+            BufferedReader bufferedReader = new BufferedReader(reader);
+            String line;
+            while (true) {
+                try {
+                    if (!((line = bufferedReader.readLine()) != null)) break;
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                str = str + line;
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        return str;
+    }
+
+    /**
+     * Create or update a note with the specified title and content.
+     *
+     * @param ttl  The title of the note.
+     * @param text The content of the note.
+     */
+    public void setfile(String ttl, String text) {
+        File temp = new File(activity.getApplicationContext().getFilesDir().toString() + "/notes/" + ttl);
+        FileWriter writer = null;
+        if(!temp.exists()){
+            try {
+                temp.createNewFile();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        try {
+            writer = new FileWriter(temp);
+            writer.write(text);
+            writer.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Delete the specified note.
+     *
+     * @param indexOf The index of the note in the list of files.
+     */
+    public void smiteFile(int indexOf) {
+        listOfFiles[indexOf].delete();
+    }
+}
+
--- app/src/main/java/edu/utsa/cs3443/finalproject/NotesActivity.java
+++ app/src/main/java/edu/utsa/cs3443/finalproject/NotesActivity.java
@@ -3,12 +3,103 @@ package edu.utsa.cs3443.finalproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class NotesActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+/**
+ * The main activity for the Notes application.
+ */
+
+
+public class NotesActivity extends AppCompatActivity implements View.OnClickListener{
+    // UI elements
+    private EditText body;
+    private EditText tile;
+    private Spinner spinner;
+
+    // Notes instance to manage notes
+    Notes notes;
+
+    // List to store note names
+    ArrayList<String> noteNames;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes);
+        // Initialize UI elements
+        spinner = findViewById(R.id.spinner);
+        body = findViewById(R.id.Body);
+        tile = findViewById(R.id.Title);
+
+        // Set default values for body and tile
+        body.setText("DEFAULT");
+        tile.setText("DEFAULT");
+
+        // Initialize Notes instance and load existing notes
+        notes = new Notes(this);
+        notes.loadNotes();
+        noteNames = notes.getnames();
+
+        // Set up the spinner with note names
+        ArrayAdapter adapter = new ArrayAdapter(this, R.layout.custom, noteNames.toArray());
+        spinner.setAdapter(adapter);
+
+        // Set up click listeners for buttons
+        setupButton(R.id.Push);
+        setupButton(R.id.Pull);
+        setupButton(R.id.Destroy);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.Pull) {
+            // Handle Pull button click
+            if (noteNames.isEmpty()) {
+                Toast.makeText(this, "YOU HAVE NO MORE FILES TO PULL", Toast.LENGTH_SHORT).show();
+            } else {
+                // Set text fields with selected note content
+                tile.setText(spinner.getSelectedItem().toString());
+                body.setText(notes.getText(noteNames.indexOf(spinner.getSelectedItem().toString())));
+                Toast.makeText(this, "Hurrah", Toast.LENGTH_SHORT).show();
+            }
+        } else if (view.getId() == R.id.Push) {
+            // Handle Push button click
+            String ttl = String.valueOf(tile.getText());
+            String text = String.valueOf(body.getText());
+            notes.setfile(ttl, text);
+            notes.loadNotes();
+            noteNames = notes.getnames();
+            ArrayAdapter adapter = new ArrayAdapter(this, R.layout.custom, noteNames.toArray());
+            spinner.setAdapter(adapter);
+        } else if (view.getId() == R.id.Destroy) {
+            // Handle Destroy button click
+            if (noteNames.isEmpty()) {
+                Toast.makeText(this, "YOU HAVE NO MORE FILES TO SMITE", Toast.LENGTH_SHORT).show();
+            } else {
+                // Remove the selected note and update the UI
+                notes.smiteFile(noteNames.indexOf(spinner.getSelectedItem().toString()));
+                notes.loadNotes();
+                noteNames = notes.getnames();
+                ArrayAdapter adapter = new ArrayAdapter(this, R.layout.custom, noteNames.toArray());
+                spinner.setAdapter(adapter);
+            }
+        }
+    }
+
+    /**
+     * Set up click listener for a button.
+     *
+     * @param ButtonID The resource ID of the button.
+     */
+    private void setupButton(int ButtonID) {
+        Button button = findViewById(ButtonID);
+        button.setOnClickListener(this);
     }
 }

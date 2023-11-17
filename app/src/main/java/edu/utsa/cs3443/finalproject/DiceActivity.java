25f3ba8ca54c64d0bd7a3845268d9b2bfa73bbc8
Onii-Fam <hipstercatz@gmail.com>
2023-11-16 22:34:52 -0600

    Added a dice history table to diceActivity and fixed some logic errors for button selections. Also did some modification on the strings for my spinners

--- app/src/main/java/edu/utsa/cs3443/finalproject/DiceActivity.java
+++ app/src/main/java/edu/utsa/cs3443/finalproject/DiceActivity.java
@@ -1,11 +1,15 @@
 package edu.utsa.cs3443.finalproject;
 
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.ToggleButton;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,7 +26,10 @@ public class DiceActivity extends AppCompatActivity {
     private Spinner advantageSpinner;
     private Button rollButton;
     private TextView resultDisplay;
-    private Button backButton;
+    //private Button backButton;
+    private ArrayList<Integer> history = new ArrayList<>();
+
+    private TableLayout tableLayout;
 
     private ArrayList<Integer> args = new ArrayList<>();
     private final Dice choice = new Dice();
@@ -40,6 +47,7 @@ public class DiceActivity extends AppCompatActivity {
         advantageSpinner = findViewById(R.id.Advantage_Spinner);
         rollButton = findViewById(R.id.Roll);
         resultDisplay = findViewById(R.id.Result_Display);
+        tableLayout = findViewById(R.id.roll_table);
 
         // Set up Spinners
         ArrayAdapter<CharSequence> diceAdapter = ArrayAdapter.createFromResource(this,
@@ -62,34 +70,83 @@ public class DiceActivity extends AppCompatActivity {
         rollButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                args = argBuilder();
-                String temp;
-                if (advantageSpinner.getSelectedItem().toString() == "Advantage") {
-                    int roll1 = choice.roll(args, diceSelect());
-                    int roll2 = choice.roll(args, diceSelect());
-                    if (roll1 >= roll2) {
-                        temp = Integer.toString(roll1);
+                if(diceSelect() != 0) {
+                    if (proficiencyToggle.isChecked() && !"Select Level".equals(levelDropDownSpinner.getSelectedItem().toString())) {
+                        args = argBuilder();
+                        String temp;
+                        if (advantageSpinner.getSelectedItem().toString() == "Advantage") {
+                            int roll1 = choice.roll(args, diceSelect());
+                            int roll2 = choice.roll(args, diceSelect());
+                            if (roll1 >= roll2) {
+                                temp = Integer.toString(roll1);
+                            } else {
+                                temp = Integer.toString(roll2);
+                            }
+                        } else if (advantageSpinner.getSelectedItem().toString() == "Disadvantage") {
+                            int roll1 = choice.roll(args, diceSelect());
+                            int roll2 = choice.roll(args, diceSelect());
+                            if (roll1 >= roll2) {
+                                temp = Integer.toString(roll2);
+                            } else {
+                                temp = Integer.toString(roll1);
+                            }
+                        } else {
+                            temp = Integer.toString(choice.roll(args, diceSelect()));
+                        }
+                        resultDisplay.setText(temp);
+                        addRoll(Integer.valueOf(temp));
+                        choice.reset();
+                        args.clear();
                     } else {
-                        temp = Integer.toString(roll2);
-                    }
-                } else if (advantageSpinner.getSelectedItem().toString() == "Disadvantage") {
-                    int roll1 = choice.roll(args, diceSelect());
-                    int roll2 = choice.roll(args, diceSelect());
-                    if (roll1 >= roll2) {
-                        temp = Integer.toString(roll2);
-                    } else {
-                        temp = Integer.toString(roll1);
+                        Toast.makeText(DiceActivity.this, "Please select a level for proficiency bonus!", Toast.LENGTH_SHORT).show();
                     }
                 } else {
-                    temp = Integer.toString(choice.roll(args, diceSelect()));
+                    Toast.makeText(DiceActivity.this, "Please select a Dice!", Toast.LENGTH_SHORT).show();
                 }
-                resultDisplay.setText(temp);
-                choice.reset();
-                args.clear();
-
             }
         });
+    }
+    /**
+     * Adds a new roll to the history, ensuring the history list contains no more than 12 elements.
+     * If adding the new roll would exceed the size limit, the oldest element is removed.
+     *
+     * @param roll The roll value to be added to the history.
+     */
+    private void historyControl(Integer roll) {
+        try {
+            this.history.add(roll);
+
+            while(this.history.size() > 12) {
+                this.history.remove(0);
+            }
+        } catch (NullPointerException e) {
+            System.out.println("History Control ArrayList Null exception");
+        }
+    }
+    private void updateTable() {
+        tableLayout.removeAllViews();
+        for (int i = 0; i < history.size(); i++) {
+            TableRow row = new TableRow(this);
+            row.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.MATCH_PARENT));
+
+            TextView textView = new TextView(this);
+            textView.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.MATCH_PARENT));
+            textView.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+            textView.setText(String.valueOf(history.get(i)));
+            textView.setTextColor(getResources().getColor(R.color.white));
+            textView.setPadding(5, 5, 5, 5);
+            textView.setGravity(Gravity.CENTER);
+            textView.setTextSize(20);
+
+            row.addView(textView);
+            tableLayout.addView(row);
+        }
+    }
+
+
+    private void addRoll(int roll) {
+        historyControl(roll); // Your method to control history size
+        updateTable(); // Update table with new history
     }
 
     private int diceSelect() {
@@ -125,7 +182,7 @@ public class DiceActivity extends AppCompatActivity {
     }
 
     private ArrayList<Integer> argBuilder() {
-        ArrayList<Integer> toReturn = new ArrayList<Integer>();
+        ArrayList<Integer> toReturn = new ArrayList<>();
         if (proficiencyToggle.isChecked()) {
             int temp = Integer.valueOf(levelDropDownSpinner.getSelectedItem().toString());
             if (temp < 5) {
@@ -139,7 +196,7 @@ public class DiceActivity extends AppCompatActivity {
             } else {
                 toReturn.add(6);
             }
-        }
+    }
         return toReturn;
     }
 }
